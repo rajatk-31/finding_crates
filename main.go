2d36b861d0116c8a4925d9ef3e1b71ddc18c11ef
rajat <rajat>
2020-02-24 16:24:32 +0530

    creating crates

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type dimensions struct {
+	length  int32
+	breadth int32
+	height  int32
+}
+
+type product struct {
+	pName  string
+	status int32
+	dimensions
+}
+
+func appendCrated(c *[]product) {
+	// c := *cd
+	// Loops to append crates
+	for i := 0; i < 5; i++ {
+		n := product{pName: "crate", status: 1, dimensions: dimensions{length: 10, breadth: 10, height: 10}}
+		*c = append(*c, n)
+	}
+	for i := 0; i < 5; i++ {
+		n := product{pName: "Big", status: 1, dimensions: dimensions{length: 22, breadth: 22, height: 22}}
+		*c = append(*c, n)
+	}
+	for i := 0; i < 5; i++ {
+		n := product{pName: "Bigger", status: 1, dimensions: dimensions{length: 50, breadth: 50, height: 50}}
+		*c = append(*c, n)
+	}
+	// return c
+}
+
+func main() {
+	fmt.Println("This is test file.")
+	allProducts := []product{}
+	appendCrated(&allProducts)
+	fmt.Println(allProducts)
+}
